96cec67374cc7da9f0567fb2d04c9034d8a27b18
“HuyTV” <tranvanhuy0202@gmail.com>
2021-03-14 21:59:24 +0700

    confg cors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 require("./src/database/db");
 const Express = require("express");
 const BodyParser = require("body-parser");
+const Cors = require("cors");
 const routes = require("./src/routes/routes");
 
 const port = process.env.PORT || 4000;
@@ -10,6 +11,7 @@ const port = process.env.PORT || 4000;
 var app = Express();
 app.use(BodyParser.json());
 app.use(BodyParser.urlencoded({ extended: true }));
+app.use(Cors());
 routes(app);
 app.listen(port, () => {
     console.log(`Listening at :${port}...`);
